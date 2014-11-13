f7b542d2462ea6329c09daa0b6739bfd0d2665d6
wsbjx <wsbjx@163.com>
2014-11-13 23:28:37 +0800

    asdf

---
+++ com.polaris.framework/com.polaris.framework.common/src/main/java/com/polaris/framework/common/utils/JsonDateTimeSerializer.java
@@ -0,0 +1,26 @@
+package com.polaris.framework.common.utils;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.JsonSerializer;
+import org.codehaus.jackson.map.SerializerProvider;
+
+/**
+ * 日期时间的格式转换
+ * 
+ * @author wang.sheng
+ * 
+ */
+public class JsonDateTimeSerializer extends JsonSerializer<Date>
+{
+	@Override
+	public void serialize(Date date, JsonGenerator gen, SerializerProvider provider) throws IOException, JsonProcessingException
+	{
+		gen.writeString(DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss"));
+	}
+
+}
