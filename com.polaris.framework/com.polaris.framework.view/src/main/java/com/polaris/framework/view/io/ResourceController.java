6b8599563317f36636ae7dbddfd60a3ad07a41f5
wsbjx <wsbjx@163.com>
2014-11-14 00:22:50 +0800

    test

---
+++ com.polaris.framework/com.polaris.framework.view/src/main/java/com/polaris/framework/view/io/ResourceController.java
@@ -0,0 +1,102 @@
+package com.polaris.framework.view.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 资源控制器
+ * 
+ * @author wang.sheng
+ * 
+ */
+@Controller
+@RequestMapping(value = "/resource")
+public class ResourceController
+{
+	Log log = LogFactory.getLog(getClass());
+
+	private final static String DEFAULT_CONTENT_TYPE = "text/html";
+
+	@Resource
+	private ResourceLoader resourceLoader;
+
+	/**
+	 * 加载指定资源
+	 * 
+	 * @param uri
+	 * @param response
+	 */
+	@RequestMapping(value = "/{uri}", method = RequestMethod.GET)
+	public void load(@PathVariable String uri, HttpServletRequest request, HttpServletResponse response)
+	{
+		log.info("load resource, uri: " + uri);
+		String contentType = getContentType(request.getServletContext(), uri);
+		response.setContentType(contentType);
+		InputStream is = null;
+		try
+		{
+			is = resourceLoader.getInputStream(uri);
+			IOUtils.copy(is, response.getOutputStream());
+		}
+		catch (IOException e)
+		{
+			log.warn("load resource failed! uri: " + uri, e);
+		}
+		finally
+		{
+			if (is != null)
+			{
+				try
+				{
+					is.close();
+				}
+				catch (IOException e)
+				{
+					log.warn("InputStream closed faled!", e);
+				}
+			}
+		}
+	}
+
+	/**
+	 * 根据URI字符串判断Reponse返回内容类型
+	 * 
+	 * @param context
+	 * @param uri
+	 * @return
+	 */
+	private static String getContentType(ServletContext context, String uri)
+	{
+		if (StringUtils.isEmpty(uri))
+		{
+			return DEFAULT_CONTENT_TYPE;
+		}
+		uri = uri.toLowerCase();
+		int index = uri.lastIndexOf(".");
+		if (index < 0)
+		{
+			return DEFAULT_CONTENT_TYPE;
+		}
+		String suffix = uri.substring(index);
+		String mimeType = context.getMimeType(suffix);
+		if (mimeType != null && !mimeType.isEmpty())
+		{
+			return mimeType;
+		}
+		return DEFAULT_CONTENT_TYPE;
+	}
+}
---
+++ com.polaris.framework/com.polaris.framework.view/src/main/java/com/polaris/framework/view/io/ResourceLoader.java
@@ -0,0 +1,39 @@
+package com.polaris.framework.view.io;
+
+import java.io.InputStream;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 资源加载器
+ * 
+ * @author wang.sheng
+ * 
+ */
+@Service
+public class ResourceLoader
+{
+	Log log = LogFactory.getLog(getClass());
+
+	private static final String RESOURCE_PATH = "META-INF/view%s";
+
+	/**
+	 * 根据路径名称获取资源输入流
+	 * 
+	 * @param uri
+	 * @return
+	 * @throws Exception
+	 */
+	public InputStream getInputStream(String uri)
+	{
+		if (StringUtils.isEmpty(uri))
+		{
+			throw new IllegalArgumentException("URI is invalid uri=" + uri + "!");
+		}
+		String resourcePath = String.format(RESOURCE_PATH, uri);
+		return Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
+	}
+}
