5d805d91168168093b8b16b2078d79375faebb2a
wsbjx <wsbjx@163.com>
2014-11-16 23:19:38 +0800

    asdf

--- com.polaris.test/src/main/webapp/js/app.js
+++ com.polaris.test/src/main/webapp/js/app.js
@@ -1,12 +1,13 @@
 Ext.require([ '*' ]);
 
 Ext.onReady(function() {
+
 	Ext.create('Ext.container.Viewport', {
 		layout : 'border',
 		items : [ {
 			region : 'north',
 			html : '<h1 class="x-panel-header">北极星智能平台联合实验室</h1>',
-			border : false,
+			border : true,
 			margin : '0',
 			split : true
 		}, {
@@ -14,7 +15,44 @@ Ext.onReady(function() {
 			collapsible : true,
 			title : '导航树',
 			width : 150,
-			split : true
+			split : true,
+			border : true,
+			xtype : "treepanel",
+			rootVisible : false,
+			store : Ext.create('Ext.data.TreeStore', {
+				root : {
+					id : "root",
+					expanded : true,
+					children : [ {
+						id : "node1",
+						text : "detention",
+						leaf : true
+					}, {
+						id : "node2",
+						text : "homework",
+						expanded : true,
+						children : [ {
+							id : "node3",
+							text : "book report",
+							leaf : true
+						}, {
+							id : "node4",
+							text : "algebra",
+							leaf : true
+						} ]
+					}, {
+						id : "node5",
+						text : "buy lottery tickets",
+						leaf : true
+					} ]
+				}
+			}),
+			listeners : {
+				itemclick : function(view, record) {
+					var nodeId = record.get("id");
+					alert("nodeId=" + nodeId);
+				}
+			}
 		}, {
 			region : 'center',
 			xtype : 'tabpanel',
