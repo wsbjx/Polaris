2587282e93c730af2b5a35ea5767950bc3b27b57
wsbjx <wsbjx@163.com>
2014-11-27 21:23:23 +0800

    asdf

--- com.polaris.framework/com.polaris.framework.view/src/main/java/com/polaris/framework/view/support/extjs/controller/ExtjsController.java
+++ com.polaris.framework/com.polaris.framework.view/src/main/java/com/polaris/framework/view/support/extjs/controller/ExtjsController.java
@@ -2,10 +2,16 @@ package com.polaris.framework.view.support.extjs.controller;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.polaris.framework.common.restful.SimpleResponse;
 import com.polaris.framework.view.support.extjs.service.ExtjsService;
+import com.polaris.framework.view.support.extjs.vo.GridFieldProperty;
 import com.polaris.framework.view.support.extjs.vo.ModelProperty;
 
 /**
@@ -18,12 +24,60 @@ import com.polaris.framework.view.support.extjs.vo.ModelProperty;
 @RequestMapping("/view/support/extjs")
 public class ExtjsController
 {
+	Log log = LogFactory.getLog(getClass());
+
 	@Resource
 	private ExtjsService extjsService;
 
-	@RequestMapping("")
-	public ModelProperty getModel()
+	/**
+	 * 获取数据对象的类型信息
+	 * 
+	 * @param className
+	 * @return
+	 */
+	@RequestMapping(value = "/model/{className}", method = RequestMethod.GET)
+	public SimpleResponse getModelInfo(@PathVariable String className)
 	{
-		return null;
+		SimpleResponse response = new SimpleResponse();
+		try
+		{
+			Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
+			ModelProperty modelProperty = extjsService.buildModelProperty(clazz);
+			response.setSuccess(true);
+			response.setData(modelProperty);
+		}
+		catch (Exception e)
+		{
+			response.setSuccess(false);
+			response.setMessage(e.getMessage());
+			log.error("getModelInfo failed!", e);
+		}
+		return response;
+	}
+
+	/**
+	 * 获取表格的字段集合
+	 * 
+	 * @param className
+	 * @return
+	 */
+	@RequestMapping(value = "/grid/{className}", method = RequestMethod.GET)
+	public SimpleResponse getGridInfo(@PathVariable String className)
+	{
+		SimpleResponse response = new SimpleResponse();
+		try
+		{
+			Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
+			GridFieldProperty[] gridFieldProperties = extjsService.buildGridFieldPropertys(clazz);
+			response.setSuccess(true);
+			response.setData(gridFieldProperties);
+		}
+		catch (Exception e)
+		{
+			response.setSuccess(false);
+			response.setMessage(e.getMessage());
+			log.error("getGridInfo failed!", e);
+		}
+		return response;
 	}
 }
